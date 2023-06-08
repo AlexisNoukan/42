26b741010c1c8182c3b3af6f7f2a7cdce19e64ac
AlexisNoukan <alexisnoukan@gmail.com>
2023-06-08 07:59:11 +0200

    C00 ex05 fonctionnel mais pas au norme

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,24 @@
+void	ft_print_comb(void)
+{
+    int i, j, k;
+
+    for (i = 0; i <= 7; i++)
+    {
+        for (j = i + 1; j <= 8; j++)
+	{
+            for (k = j + 1; k <= 9; k++)
+	    {
+                ft_putchar(i + '0');
+                ft_putchar(j + '0');
+                ft_putchar(k + '0');
+
+                if (i != 7 || j != 8 || k != 9)
+		{
+                    ft_putchar(',');
+                    ft_putchar(' ');
+                }
+            }
+        }
+    }
+    ft_putchar('\n');
+}
