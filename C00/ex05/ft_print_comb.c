69b7e420af198eee9383f739dbd8b9cd8a63bdda
AlexisNoukan <alexisnoukan@gmail.com>
2023-06-08 19:45:16 +0200

    c00 ex05

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -9,6 +9,20 @@
 /*   Updated: 2023/06/08 13:33:26 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
+
+void ft_putchar(char t)
+{
+    write(1, &t, 1);
+}
+void ft_display(int o, int k, int l)
+{
+    ft_putchar(o);
+    ft_putchar(k);
+    ft_putchar(l);
+    ft_putchar(',');
+    ft_putchar(' ');
+}
 void    ft_print_comb(void)
 {
     int o;
@@ -19,30 +33,22 @@ void    ft_print_comb(void)
     k = '1';
     l = '2';
     
-    while (o <= ('6' + 1))
+    while (o <= '7')
     {
-        while (k <= ('7' + 1))
+        while (k <= '8')
         {
-            while (l <= ('8' + 1))
+            while (l <= '9')
             {
                 ft_display(o, k, l);
                 l++ ; 
             }
-            l = ++k ;
+            l = ++k + 1;
         }
-        k = ++o ;
+        k = ++o + 1;
     }
 }
-void ft_putchar(char t)
-{
-    write(1, &t, 1);
-}
 
-void ft_display(int o, int k, int l)
-{
-    ft_putchar(o);
-    ft_putchar(k);
-    ft_putchar(l);
-    ft_putchar(',');
-    ft_putchar(' ');
+int main(){
+	ft_print_comb();
+	return 0;
 }
