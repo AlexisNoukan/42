6501bea0f3230252d24b8de71954997e32572ab5
Alexis Noukan <anoukan@k1r4p16.42mulhouse.fr>
2023-06-13 15:17:28 +0200

    C02 ex04

---
+++ C02/ex03/ft_str_is_numeric.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anoukan <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/13 14:02:36 by anoukan           #+#    #+#             */
+/*   Updated: 2023/06/13 14:33:29 by anoukan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <stdio.h>
+int	ft_str_is_numeric(char *str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i])
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+		{
+			return (1);
+		}
+		++i;
+	}
+	return (0);
+}
+int	main()
+{
+	char str[] = "1234";
+	int	resultat =  ft_str_is_numeric(str);
+	char str2[] = "hello";
+    int resultat2 =  ft_str_is_numeric(str2);
+	char str3[] = " ";
+    int resultat3 =  ft_str_is_numeric(str3);
+	printf("%d", resultat);
+	printf("%d", resultat2);
+	printf("%d", resultat3);
+}
+
--- C02/ex04/ft_str_is_lowercase.c
+++ C02/ex04/ft_str_is_lowercase.c
@@ -6,27 +6,34 @@
 /*   By: anoukan <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 13:23:18 by anoukan           #+#    #+#             */
-/*   Updated: 2023/06/13 13:24:06 by anoukan          ###   ########.fr       */
+/*   Updated: 2023/06/13 14:50:12 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-int	ft_str_is_alpha(char *str)
+int ft_str_is_lowercase(char *str)
 {
-	int	i;
+    int i;
 
-	i = 0;
-	while(str[i] <= 'a' && str[i] <= 'z')
-	{
-		return (0);
-		++i;
-	}
-	return (1);
+    i = 0;
+    while(str[i])
+    {	if (str[i] <= 'a' && str[i] <= 'z')
+		{
+			return (1);
+		}
+        ++i;
+    }
+    return (0);
 }
 int	main()
 {
-	char str[] = "Hello";
-	int	resultat =  ft_str_is_alpha(str);
+	char str[] = "1234";
+	int	resultat =  ft_str_is_lowercase(str);
+	char str2[] = "hello";
+    int resultat2 =  ft_str_is_lowercase(str2);
+	char str3[] = "";
+    int resultat3 =  ft_str_is_lowercase(str3);
 	printf("%d", resultat);
-
+	printf("%d", resultat2);
+	printf("%d", resultat3);
 }
