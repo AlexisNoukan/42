cb1703ee1aec5826a022a46f663de5149b64be60
Alexis Noukan <anoukan@k1r4p10.42mulhouse.fr>
2023-06-15 15:46:37 +0200

    c02 ex03 juste

--- C02/ex03/ft_str_is_numeric.c
+++ C02/ex03/ft_str_is_numeric.c
@@ -6,19 +6,23 @@
 /*   By: anoukan <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 14:02:36 by anoukan           #+#    #+#             */
-/*   Updated: 2023/06/15 15:38:10 by anoukan          ###   ########.fr       */
+/*   Updated: 2023/06/15 15:45:57 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
+#include <stdio.h>
 int	ft_str_is_numeric(char *str)
 {
 	int	i;
 
 	i = 0;
+	if (str[i] == '\0' )
+	{
+		return (1);
+	}
 	while (str[i])
 	{
-		if ((str[i] >= '0' && str[i] <= '9)' || (str[i] = "\0"))
+		if (str[i] >= '0' && str[i] <= '9')
 		{
 			return (1);
 		}
