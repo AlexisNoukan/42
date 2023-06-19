69dfbc56054b652ad1de5552bb2fe45d2eddd2b4
Alexis Noukan <anoukan@k1r4p11.42mulhouse.fr>
2023-06-19 17:53:34 +0200

    c03 ex04 correction

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -6,41 +6,41 @@
 /*   By: anoukan <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 12:11:08 by anoukan           #+#    #+#             */
-/*   Updated: 2023/06/19 15:11:00 by anoukan          ###   ########.fr       */
+/*   Updated: 2023/06/19 17:52:59 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <string.h>
+//#include <string.h>
 
 char	*ft_strstr(char *str, char *to_find)
 {
 	int	i;
-	int j;
+	int	j;
 	int	n;
 
 	i = 0;
 	while (str[i])
 	{
-		 n = i;
-		 j = 0;
-		 while (str[n] == to_find[j] && str[n] && to_find[j])
-		 {
+		n = i;
+		j = 0;
+		while (str[n] == to_find[j] && str[n] && to_find[j])
+		{
 			n++;
 			j++;
-		 }
-		 if (to_find[j] == '\0')
-			 return (str + i);
-		 i++;
+		}
+		if (to_find[j] == '\0')
+			return (str + i);
+		i++;
 	}
 	return (NULL);
 }
-int	main()
+/*int	main()
 {
-	char	s1[] = "Hello";
-	char	s2[] = "Hello";
+	char	s1[] = "Hello World";
+	char	s2[] = "llo";
 	printf("%s\n", ft_strstr(s1, s2));
-	char    s3[] = "Hello";
-    char    s4[] = "Hello";
+	char    s3[] = "Hello World";
+    char    s4[] = "llo";
 	printf("%s", strstr(s3, s4));
-}
+}*/
