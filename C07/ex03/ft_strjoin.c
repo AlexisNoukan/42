616c38b2715ce94722791f4a43d92c34166e618f
Alexis Noukan <anoukan@k1r4p11.42mulhouse.fr>
2023-06-24 21:29:23 +0200

    C07 ex03

--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -6,12 +6,86 @@
 /*   By: anoukan <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 19:26:58 by anoukan           #+#    #+#             */
-/*   Updated: 2023/06/24 19:40:05 by anoukan          ###   ########.fr       */
+/*   Updated: 2023/06/24 21:28:40 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strjoin(int size, char **strs, char *sep)
+#include <stdlib.h>
+#include <stdio.h>
+
+char *ft_strcat(char *dest, char *src)
 {
-	if (size == 0)
-		return (0);
+    int i;
+
+	i = 0;
+    while (dest[i] != '\0')
+    {
+        i++;
+    }
+
+    int j;
+
+	j = 0;
+    while (src[j] != '\0')
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    dest[i] = '\0';
+
+    return dest;
+}
+
+int ft_strlen(char *str)
+{
+    int len;
+
+	len = 0;
+    while (str[len] != '\0')
+    {
+        len++;
+    }
+    return len;
+}
+
+char *ft_strjoin(int size, char **strs, char *sep)
+{
+    if (size <= 0)
+        return (0);
+
+    int total_length;
+    int sep_length;
+    int i;
+
+	total_len = 0;
+	sep_length = ft_strlen(sep);
+	i = 0;
+	while (i < size)
+    {
+        total_length += ft_strlen(strs[i]);
+        i++;
+    }
+
+    int total_sep_length = (size - 1) * sep_length;
+    char *result = (char *)malloc(total_length + total_sep_length + 1);
+    if (result == '\0')
+        return (0);
+
+    ft_strcat(result, strs[0]);
+
+    i = 1;
+    while (i < size)
+    {
+        ft_strcat(result, sep);
+        ft_strcat(result, strs[i]);
+        i++;
+    }
+
+    return result;
+}
+
+int	main(int argc, char **argv)
+{
+	
 }
