7b4ceaaa6925f81eb775332544f170d8477d7b6a
AlexisNoukan <alexisnoukan@gmail.com>
2023-06-25 09:55:45 +0200

    c08 ex00

---
+++ C08/ex00/ft.h
@@ -0,0 +1,5 @@
+return  void  ft_putchar(char c);
+return  void  ft_swap(int *a,int *b);
+return  void  ft_putstr(char *str);
+return  int ft_strlen(char *str);
+return  int ft_strcmp(char *s1,char *s2);
